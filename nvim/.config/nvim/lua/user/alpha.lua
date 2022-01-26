f15448f64318fa2ee6bdc5697ab97c0dcdb6031d
Piyush Kumar <piyusheverhard@gmail.com>
2022-01-26 17:36:09 +0530

    minor changes

--- nvim/.config/nvim/lua/user/alpha.lua
+++ nvim/.config/nvim/lua/user/alpha.lua
@@ -31,7 +31,7 @@ local function footer()
 	-- local fortune = handle:read("*a")
 	-- handle:close()
 	-- return fortune
-	return " piyusheverhard"
+	return " Welcome piyusheverhard "
 end
 
 dashboard.section.footer.val = footer()
