dd6361fe5a64c8b2c1c6e008795ca956b7f2e6c0
Piyush Kumar <piyusheverhard@gmail.com>
2022-06-05 10:37:03 +0530

    Updated template

--- cpbooster/.config/cpbooster/template.cpp
+++ cpbooster/.config/cpbooster/template.cpp
@@ -1,130 +1,24 @@
-
-// created by "piyusheverhard"
-//@ 国立工科大学パトナ校
-
-/*---------------------------------------------------------------------*/
-
+#ifdef _genjutsu
+#include "Sharingan.h"
+#else
 #include <bits/stdc++.h>
+#define deb(x) 69
 using namespace std;
-/* #include <ext/pb_ds/assoc_container.hpp> */
-/* #include <ext/pb_ds/tree_policy.hpp> */
-/* using namespace __gnu_pbds; */
-
-#define int long long
+#endif
 #define nline "\n"
-#define mp make_pair
-#define pb push_back
-#define ppb pop_back
-#define ff first
-#define ss second
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
-#define ub upper_bound
-#define lb lower_bound
-#define bsrch binary_search
-#define mina *min_element
-#define maxa *max_element
-#define gcd(a, b) __gcd(a, b)
-#define lcm(a, b) (a * b / gcd(a, b))
-
-#define rep(i, a, b) for (int i = a; i < b; i++)
-
-typedef pair<int, int> pii;
-typedef set<int> si;
-typedef set<pii> spii;
-typedef vector<pii> vpii;
-typedef vector<int> vi;
-typedef vector<vector<int>> vvi;
-/* typedef tree<pii, null_type, less<pii>, rb_tree_tag, */
-/*              tree_order_statistics_node_update> */
-/*     oset; // ordered_set */
-/* // member functions : */
-/* // 1. order_of_key(k) : number of elements strictly lesser than k. */
-/* // 2. find_by_order(k) : k-th element in the oset. */
-
-#define PI 3.141592653589793
-#define MOD 1000000007
-#define INF 1000000000000000000LL
-
-/*----------------------------DEBUG-TEMPLATE---------------------------------------*/
-
-#ifdef _genjutsu
-#define deb(x) cerr << #x << ": ", _print(x), cerr << nline
-#else
-#define deb(x) 69
-#endif
-
-void _print(int x) { cerr << x; }
-void _print(char x) { cerr << x; }
-void _print(string x) { cerr << x; }
-void _print(double x) { cerr << x; }
-void _print(long double x) { cerr << x; }
-template <typename T, typename V> void _print(pair<T, V> p);
-template <typename T> void _print(vector<T> v);
-template <typename T> void _print(multiset<T> m);
-template <typename T> void _print(set<T> s);
-template <typename T, typename V> void _print(map<T, V> m);
-template <typename T, typename V> void _print(pair<T, V> p) {
-  cerr << "{";
-  _print(p.ff);
-  cerr << ", ";
-  _print(p.ss);
-  cerr << "}";
-}
-template <typename T> void _print(vector<T> v) {
-  cerr << '[';
-  for (T i : v) {
-    _print(i);
-    cerr << ' ';
-  }
-  cerr << ']';
-}
-template <typename T> void _print(set<T> s) {
-  cerr << '[';
-  for (T i : s) {
-    _print(i);
-    cerr << ' ';
-  }
-  cerr << ']';
-}
-template <typename T> void _print(multiset<T> m) {
-  cerr << '[';
-  for (T i : m) {
-    _print(i);
-    cerr << ' ';
-  }
-  cerr << ']';
-}
-template <typename T, typename V> void _print(map<T, V> m) {
-  cerr << '[';
-  for (auto i : m) {
-    _print(i);
-    cerr << ' ';
-  }
-  cerr << ']';
-}
-
 /*---------------------------------------------------------------------*/
 
-void SHIT_HERE_WE_GO_AGAIN() {
-
-
-}
+void SHIT_HERE_WE_GO_AGAIN() {}
 
 int32_t main() {
-
-  /* #ifdef ONLINE_JUDGE */
-  /*   freopen("input.txt", "r", stdin); */
-  /*   freopen("output.txt", "w", stdout); */
-  /* #endif */
-
   ios_base::sync_with_stdio(false);
   cin.tie(0);
-
   int t = 1;
   cin >> t;
   for (int i = 1; i <= t; i++) {
-    /* cout << "Case #" << i << ": "; */
+    // cout << "Case #" << i << ": ";
     SHIT_HERE_WE_GO_AGAIN();
   }
 }
