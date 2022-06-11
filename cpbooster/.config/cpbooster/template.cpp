f613f2287e65a87d14798c115fd8cb981fc5249b
Piyush Kumar <piyusheverhard@gmail.com>
2022-06-11 13:40:04 +0530

    minor changes

--- cpbooster/.config/cpbooster/template.cpp
+++ cpbooster/.config/cpbooster/template.cpp
@@ -2,23 +2,19 @@
 #include "Sharingan.h"
 #else
 #include <bits/stdc++.h>
-#define deb(x) 69
+#define deb(x) 
 using namespace std;
 #endif
-#define nline "\n"
-#define all(x) x.begin(), x.end()
-#define rall(x) x.rbegin(), x.rend()
 /*---------------------------------------------------------------------*/
 
 void SHIT_HERE_WE_GO_AGAIN() {}
 
-int32_t main() {
+int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(0);
   int t = 1;
   cin >> t;
   for (int i = 1; i <= t; i++) {
-    // cout << "Case #" << i << ": ";
     SHIT_HERE_WE_GO_AGAIN();
   }
 }
